14cbbd0ae356a8d3c6def5568a8fdddcb0dd90b3
LipePTJ <felipepereirateles71@gmail.com>
2026-05-21 09:50:40 -0300

    Create adocaooo.java

---
+++ adocaooo.java
@@ -0,0 +1,12 @@
+public class Adocaooo {
+
+    private String nome;
+    private int idade;
+    private Tipo tipo;
+    private String raca;
+    private String endereco;
+    private int telefone;
+
+    public Adocaooo(){
+
+    }
