0447e5304c5ef11925f9c852203038ce93537d9f
LipePTJ <felipepereirateles71@gmail.com>
2026-05-21 09:49:47 -0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,97 +1,98 @@
 import javax.swing.*;
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
-void main() {
+public class Main {
+    public static void main(String[] args) {
 
-        JFrame tela = new JFrame("Adoção");
-        tela.setSize(500, 400);
+        JFrame tela = new JFrame("Cadastro para Adoção");
+        tela.setSize(600, 520);
         tela.setLayout(null);
 
         JLabel labelNome = new JLabel("Nome");
-        labelNome.setBounds(20, 20, 150, 30);
+        labelNome.setBounds(20, 20, 150, 25);
         tela.add(labelNome);
 
         JTextField nome = new JTextField();
-        nome.setBounds(20, 50, 440, 30);
+        nome.setBounds(20, 45, 250, 30);
         tela.add(nome);
 
         JLabel labelIdade = new JLabel("Idade");
-        labelIdade.setBounds(20, 90, 150, 30);
+        labelIdade.setBounds(20, 85, 150, 25);
         tela.add(labelIdade);
 
-        JTextField Idade = new JTextField();
-        Idade.setBounds(20, 120, 440, 30);
-        tela.add(Idade);
+        JTextField idade = new JTextField();
+        idade.setBounds(20, 110, 250, 30);
+        tela.add(idade);
 
         JLabel labelTipo = new JLabel("Tipo");
-        labelTipo.setBounds(20, 160, 150, 30);
+        labelTipo.setBounds(20, 150, 150, 25);
         tela.add(labelTipo);
 
-        JTextField Tipo = new JTextField();
-        Tipo.setBounds(20, 190, 440, 30);
-        tela.add(Tipo);
+        JComboBox<Tipo> textTipo = new JComboBox<>(Tipo.values());
+        textTipo.setBounds(20, 175, 250, 30);
+        tela.add(textTipo);
 
         JLabel labelRaca = new JLabel("Raça");
-        labelRaca.setBounds(20, 230, 150, 30);
+        labelRaca.setBounds(20, 215, 150, 25);
         tela.add(labelRaca);
 
-        JTextField Raca = new JTextField();
-        Raca.setBounds(20, 260, 440, 30);
-        tela.add(Raca);
+        JTextField raca = new JTextField();
+        raca.setBounds(20, 240, 250, 30);
+        tela.add(raca);
 
-        JLabel labelEndereco = new JLabel("endereço");
-        labelEndereco.setBounds(20, 300, 150, 30);
+        JLabel labelEndereco = new JLabel("Endereço");
+        labelEndereco.setBounds(20, 280, 150, 25);
         tela.add(labelEndereco);
 
-        JTextField Endereco = new JTextField();
-        Endereco.setBounds(20, 330, 440, 30);
-        tela.add(Endereco);
+        JTextField endereco = new JTextField();
+        endereco.setBounds(20, 305, 250, 30);
+        tela.add(endereco);
 
         JLabel labelTelefone = new JLabel("Telefone");
-        labelTelefone.setBounds(20, 370, 150, 30);
+        labelTelefone.setBounds(20, 345, 150, 25);
         tela.add(labelTelefone);
 
-        JTextField Telefone = new JTextField();
-        Telefone.setBounds(20, 400, 440, 30);
-        tela.add(Telefone);
+        JTextField telefone = new JTextField();
+        telefone.setBounds(20, 370, 250, 30);
+        tela.add(telefone);
 
-
-        JButton enviar = new JButton("enviar");
-        enviar.setBounds(175, 470, 150, 40);
+        JButton enviar = new JButton("Enviar");
+        enviar.setBounds(20, 420, 150, 40);
         tela.add(enviar);
 
+        enviar.addActionListener(actionEvent -> {
 
-    enviar.addActionListener(e ->{
-        String sql = "INSERT INTO animal(Nome, Idade, Tipo, Raca, Endereco, Telefone) VALUES (?,?,?,?,?,?)";
-        String nomeAnimal = nome.getText();
-        String idadeAnimal = Idade.getText();
-        String tipoAnimal = Tipo.getText();
-        String racaAnimal = Raca.getText();
-        String enderecoAnimal = Endereco.getText();
-        String telefoneAnimal = Telefone.getText();
+            String nomeAnimal = nome.getText();
+            String idadeAnimal = idade.getText();
+            String tipoAnimal = textTipo.getSelectedItem().toString();
+            String racaAnimal = raca.getText();
+            String enderecoAnimal = endereco.getText();
+            String telefoneAnimal = telefone.getText();
 
-        try {
-            Connection conexao = Conexao.conectar();
-            PreparedStatement ps = conexao.prepareStatement(sql);
+            String sql = "INSERT INTO pets(nome, idade, tipo, raca, endereco, telefone) VALUES (?,?,?,?,?,?)";
 
-            ps.setString(1, nomeAnimal);
-            ps.setString(2, idadeAnimal);
-            ps.setString(3, tipoAnimal);
-            ps.setString(4, racaAnimal);
-            ps.setString(5, enderecoAnimal);
-            ps.setString(6, telefoneAnimal);
-            ps.executeUpdate();
+            try {
+                Connection conexao = Conexao.conectar();
+                PreparedStatement ps = conexao.prepareStatement(sql);
 
-            JOptionPane.showMessageDialog(null, "Animal salvo com sucesso!");
-            ps.close();
-            conexao.close();
+                ps.setString(1, nomeAnimal);
+                ps.setString(2, idadeAnimal);
+                ps.setString(3, tipoAnimal);
+                ps.setString(4, racaAnimal);
+                ps.setString(5, enderecoAnimal);
+                ps.setString(6, telefoneAnimal);
+                ps.executeUpdate();
+                JOptionPane.showMessageDialog(null, "Animal salvo com sucesso!");
+                ps.close();
+                conexao.close();
 
-        } catch (Exception ex) {
-            throw new RuntimeException(ex);
-        }
-    });
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
+        });
 
-    tela.setVisible(true);
+        tela.setVisible(true);
+    }
 }
